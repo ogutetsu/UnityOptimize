d4f228ecca8512de6bb146513964e32444ec74fa
og <ogugu@test.com>
2020-02-10 11:11:38 +0900

    [add] GetCompPerformanceクラスを追加
    GetComponentの取得方法別のパフォーマンス測定

---
+++ Assets/Scripts/GetCompPerformance.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetCompPerformance : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num = 1000000;
+
+        Transform test;
+        using(new CustomTimer("GetComponent(string)", num))
+        {
+            for(var i = 0; i < num; i++)
+            {
+                test = (Transform)GetComponent("Transform");
+            }
+        }
+
+        using(new CustomTimer("GetComponent<ComponentName>", num))
+        {
+            for(var i = 0; i < num; i++)
+            {
+                test = GetComponent<Transform>();
+            }
+        }
+
+        using(new CustomTimer("GetComponent(typeof(ComponentName))", num))
+        {
+            for (var i = 0; i < num; i++)
+            {
+                test = (Transform)GetComponent(typeof(Transform));
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
