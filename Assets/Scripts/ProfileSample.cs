4cbe70f1fbbeeb1695a44463072946b23d7e6061
og <ogugu@test.com>
2020-02-10 10:41:16 +0900

    [add] ProfileSampleクラスを追加
    Profilerクラスの使用方法

---
+++ Assets/Scripts/ProfileSample.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+public class ProfileSample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DoSomething();
+    }
+
+
+    void DoSomething()
+    {
+        Profiler.BeginSample("Profiler Sample");
+        List<int> listOfInts = new List<int>();
+        for(int i = 0; i < 1000000; ++i)
+        {
+            listOfInts.Add(i);
+        }
+        Profiler.EndSample();
+
+    }
+
+
+}
