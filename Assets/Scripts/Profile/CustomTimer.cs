a030e768bc82af6ab1f4407531f8a6bb3fb3446e
og <ogugu@test.com>
2020-02-10 11:05:48 +0900

    [add] CustomTimerクラスを追加

---
+++ Assets/Scripts/Profile/CustomTimer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+public class CustomTimer : IDisposable
+{
+    private string _timerName;
+    private int _numTests;
+    private Stopwatch _watch;
+
+    public CustomTimer(string timerName, int numTests)
+    {
+        _timerName = timerName;
+        _numTests = numTests;
+        if(_numTests <= 0)
+        {
+            _numTests = 1;
+        }
+        _watch = Stopwatch.StartNew();
+    }
+
+    public void Dispose()
+    {
+        _watch.Stop();
+        float ms = _watch.ElapsedMilliseconds;
+        UnityEngine.Debug.Log(string.Format("{0} finished: {1:0.00} " +
+            "milliseconds total, {2:0.000000} milliseconds per-test " +
+            "for {3} tests", _timerName, ms, ms / _numTests, _numTests));
+    }
+}
