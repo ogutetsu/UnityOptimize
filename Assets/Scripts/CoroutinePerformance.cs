af9cdf9df6d17c2c8edf501a8d307148339a2259
og <ogugu@test.com>
2020-02-10 11:33:02 +0900

    [add] CoroutinePerformanceクラスを追加
    Updateを使用せずにコルーチンでのUpdate関数の置き換えテスト
    Coroutineで実装した場合と、InvokeRepeatingを使用した場合での比較

---
+++ Assets/Scripts/CoroutinePerformance.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutinePerformance : MonoBehaviour
+{
+
+    private int count = 0;
+    private float delay = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num = 100000;
+        using (new CustomTimer("Coroutine Test ", num))
+        {
+            for(int i = 0; i < num; i++)
+                StartCoroutine(ProcessCoroutine());
+        }
+
+        //InvokeRepeatingの方が若干速い
+        using (new CustomTimer("InvokeRepeating Test ", num))
+        {
+            for (int i = 0; i < num; i++)
+                InvokeRepeating("Process", 0f, delay);
+        }
+    }
+
+
+
+    void Process()
+    {
+        count++;
+    }
+
+    IEnumerator ProcessCoroutine()
+    {
+        while(true)
+        {
+            Process();
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+
+}
