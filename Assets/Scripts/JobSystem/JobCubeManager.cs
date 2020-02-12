b80130c68468c864941038c47eba2e35a016c8b6
og <ogugu@test.com>
2020-02-12 15:13:23 +0900

    [add] キューブを生成して回転させるJobSystem

---
+++ Assets/Scripts/JobSystem/JobCubeManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+using Unity.Jobs;
+using UnityEngine.Jobs;
+
+public class JobCubeManager : MonoBehaviour
+{
+
+    public float cubeSpacing = 0.1f;
+    public int width = 10;
+    public int height = 10;
+
+    public GameObject cubePrefab;
+
+    TransformAccessArray transformAccessArray;
+    JobHandle jobHandle;
+    NativeList<float> speeds;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transformAccessArray = new TransformAccessArray(0, -1);
+        speeds = new NativeList<float>(1, Allocator.Persistent);
+        SpawnCubes();
+    }
+
+    void SpawnCubes()
+    {
+        Vector3 position = new Vector3();
+        while(position.x < width)
+        {
+            while(position.y < height)
+            {
+                var newCube = Instantiate(cubePrefab);
+                newCube.transform.position = position;
+                position = new Vector3(position.x, position.y + cubeSpacing, 0f);
+                transformAccessArray.Add(newCube.transform);
+                speeds.Add(Random.Range(25.0f, 50.0f));
+            }
+            position = new Vector3(position.x + cubeSpacing, 0f, 0f);
+        }
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        jobHandle.Complete();
+        if(jobHandle.IsCompleted)
+        {
+            var rotatorJob = new RotatorJob()
+            {
+                deltaTime = Time.deltaTime,
+                speeds = speeds
+            };
+            jobHandle = rotatorJob.Schedule(transformAccessArray);
+            JobHandle.ScheduleBatchedJobs();
+
+        }
+    }
+}
---
+++ Assets/Scripts/JobSystem/RotatorJob.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Jobs;
+
+public struct RotatorJob : IJobParallelForTransform
+{
+
+    [ReadOnly]
+    public NativeList<float> speeds;
+    [ReadOnly]
+    public float deltaTime;
+
+    public void Execute(int index, TransformAccess transform)
+    {
+        Vector3 currentRotation = transform.rotation.eulerAngles;
+        currentRotation.y += speeds[index] * deltaTime;
+        transform.rotation = Quaternion.Euler(currentRotation);
+
+    }
+}
