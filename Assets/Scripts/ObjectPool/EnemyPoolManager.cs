227dabac5c13ba46a5ef2f4f9c51da42b02bf91e
og <ogugu@test.com>
2020-02-12 11:59:34 +0900

    [add] EnemyPoolManagerの追加
    ObjectPoolのサンプルコード

---
+++ Assets/Scripts/ObjectPool/EnemyPoolManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyPool : IPoolableObject
+{
+    public void New()
+    {
+        Debug.Log("New Enemy");
+    }
+
+    public void Respawn()
+    {
+        Debug.Log("Respawn Enemy");
+    }
+}
+
+
+
+public class EnemyPoolManager : MonoBehaviour
+{
+
+    private ObjectPool<EnemyPool> Enemyies = new ObjectPool<EnemyPool>(50);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("respawn");
+
+        for(int i = 0; i < 50; i++)
+        {
+            EnemyPool e = Enemyies.Spawn();
+        }
+
+
+        Debug.Log("new enemy");
+
+        for (int i = 0; i < 50; i++)
+        {
+            EnemyPool e = Enemyies.Spawn();
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
