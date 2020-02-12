fc3d1cec39c7791d1536df1e625a55500f6647e3
og <ogugu@test.com>
2020-02-12 14:40:07 +0900

    [add] JobSystemの追加
    単純なJobSystem

---
+++ Assets/Scripts/JobSystem/SimpleJob.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Jobs;
+using UnityEngine;
+using Unity.Collections;
+
+public struct SimpleJob : IJob
+{
+    public float number;
+
+    public NativeArray<float> data;
+
+    public void Execute()
+    {
+        data[0] += number;
+    }
+
+}
---
+++ Assets/Scripts/JobSystem/SimpleJobRunner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using Unity.Collections;
+using Unity.Jobs;
+
+public class SimpleJobRunner : MonoBehaviour
+{
+
+    public float number = 5;
+
+    private NativeArray<float> data;
+
+    private JobHandle simpleJobHandle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        data = new NativeArray<float>(1, Allocator.Persistent);
+        data[0] = 2;
+        SimpleJob simpleJob = new SimpleJob
+        {
+            number = this.number,
+            data = this.data
+        };
+
+        simpleJobHandle = simpleJob.Schedule();
+
+        JobHandle.ScheduleBatchedJobs();
+
+        simpleJobHandle.Complete();
+
+        if(simpleJobHandle.IsCompleted)
+        {
+            Debug.Log(simpleJob.data[0]);
+        }
+
+        data.Dispose();
+
+    }
+
+}
